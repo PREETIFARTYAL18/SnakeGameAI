c025fcebaf9d1c9d786848c4786fd82bc72af7dc
Preeti Fartyal <preetifartyal13@gmail.com>
2026-01-18 18:37:10 +0530

    Initial commit - Snake AI using DQN

---
+++ snake_gameai.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+from snake_game import SnakeGame
+from agent import Agent
+plt.ion()
+
+def plot(scores, mean_scores):
+    plt.clf()
+    plt.title('Training Progress')
+    plt.xlabel('Number of Games')
+    plt.ylabel('Score')
+    plt.plot(scores)
+    plt.plot(mean_scores)
+    plt.ylim(ymin=0)
+    plt.show(block=False)
+    plt.pause(0.1)
+def train():
+    scores = []
+    mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGame()
+
+    while True:
+        state_old = agent.get_state(game)
+
+        final_move = agent.get_action(state_old)
+
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                agent.model.save()
+
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+            scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            mean_scores.append(mean_score)
+            plot(scores, mean_scores)
+if __name__ == '__main__':
+    train()
